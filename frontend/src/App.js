6693789dd68cbb10bcbd36f85a9e82b8b5302197
Dexter Cooke <afrodexx@gmail.com>
2025-06-29 20:29:37 -0400

    Save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -16,25 +18,20 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images])
       })
       .catch((err) => {
         console.log(err);
-      })
-      setWord('');
-    }
+      });
+    setWord("");
+  };
 
-    return (
-      <div className="App">
-        <Header title="Images Gallery" />
-        <Search
-          word={word}
-          setWord={setWord}
-          handleSubmit={handleSearchSubmit}
-        />
-      </div>
-    );
-
-}
+  return (
+    <div className="App">
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+    </div>
+  );
+};
 
 export default App;
